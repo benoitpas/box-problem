942bbb93e45625ac3154e60199a29e7e39d8bfa5
benoitpas <bpasquereau@gmail.com>
2024-10-07 19:58:42 +0100

    adding a failing test

--- src/main/scala/BoxProblem.scala
+++ src/main/scala/BoxProblem.scala
@@ -1,3 +1,8 @@
+case class Cell(col: Int, row: Int)
+
 object BoxProblem {
   def solve(nbCol: Int, nbRow: Int) = (0, 0, 1)
+
+  def generateCombinations(nbCol: Int, nbRow: Int): List[(Cell, Cell)] =
+    List()
 }
--- src/test/scala/TestBoxProblem.scala
+++ src/test/scala/TestBoxProblem.scala
@@ -1,7 +1,19 @@
 import org.junit.Test
 import org.junit.Assert._
+import javax.swing.CellEditor
 
-object tests {
+class TestBoxProblem:
   @Test def test5x3(): Unit =
     assertEquals((0, 0, 1), BoxProblem.solve(5, 3))
-}
+
+  @Test def testGenerateCombinations(): Unit =
+    val nbCol = 2
+    val nbRow = 3
+    val c = BoxProblem.generateCombinations(nbCol, nbRow)
+    val nbCell = nbCol * nbRow
+    val l = c
+      .map((c1: Cell, c2: Cell) =>
+        (c1.col - 1) + (c1.row - 1) * nbRow + (c2.col - 1) * nbCell + (c2.row - 1) * nbRow * nbCell
+      )
+      .sorted
+    assertEquals(Seq.range(1, nbCell * nbCell + 1).toList, l)
