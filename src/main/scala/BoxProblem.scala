3e42f822b4267db8ea3960c61f06d60b0e07a9e2
benoitpas <bpasquereau@gmail.com>
2024-10-07 20:14:51 +0100

    make test pass

--- src/main/scala/BoxProblem.scala
+++ src/main/scala/BoxProblem.scala
@@ -4,5 +4,13 @@ object BoxProblem {
   def solve(nbCol: Int, nbRow: Int) = (0, 0, 1)
 
   def generateCombinations(nbCol: Int, nbRow: Int): List[(Cell, Cell)] =
-    List()
+    val oneCell = (for
+      col <- 1 to nbCol
+      row <- 1 to nbRow
+    yield Cell(col, row)).toList
+
+    for
+      c1 <- oneCell
+      c2 <- oneCell
+    yield (c1, c2)
 }
